4c1f094c951afe3da18a3557c237989d9a4c8766
Phil Kunz <github@phil.today>
2015-10-21 17:04:22 +0200

    added travis

--- ts/index.js
+++ ts/index.js
@@ -1,9 +1,6 @@
 /// <reference path="typings/tsd.d.ts" />
 var path = require("path");
 var open = require("open");
-
-var smarturl:any = {};
-
-
-
+var smarturl = {};
 module.exports = smarturl;
+//# sourceMappingURL=index.js.map
