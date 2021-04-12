ea9b67a328cf5d4795fe8934208f296431d5f01a
Phil Kunz <gitlab@philkunz.com>
2021-04-12 19:41:03 +0000

    fix(core): update

--- ts/smarturl.classes.smarturl.ts
+++ ts/smarturl.classes.smarturl.ts
@@ -2,6 +2,6 @@ import * as plugins from './smarturl.plugins';
 
 export class Smarturl {
   public parseUrl(urlArg: string) {
-    return plugins.url.parse(urlArg);
+    return new plugins.url.URL(urlArg);
   }
 }
